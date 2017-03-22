599335f29e19f6b5c49c0c5f6264f4ea00cb0cc3
Administrator <Administrator@ZY-20160907TPKV>
2017-03-22 19:18:25 +0800

    spring hessian server add json convert support

--- sprhsnserver/src/main/java/org/pub/hello/impl/HelloServiceImpl.java
+++ sprhsnserver/src/main/java/org/pub/hello/impl/HelloServiceImpl.java
@@ -2,25 +2,17 @@ package org.pub.hello.impl;
 
 import org.pub.hello.HelloService;
 import org.pub.hello.bean.User;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Component;
 
-@Controller
-@RequestMapping(value="/hello")
+@Component
 public class HelloServiceImpl implements HelloService {
 
 	@Override
-	@RequestMapping(value="/sayhello",method={RequestMethod.POST,RequestMethod.GET})
-	@ResponseBody
 	public String sayHello(String name) {
 		return "Hello " + name;
 	}
 
 	@Override
-	@RequestMapping(value="/saygoodbye",method={RequestMethod.POST,RequestMethod.GET})
-	@ResponseBody
 	public String sayGoodbye(User user) {
 		String name = user.getName();
 		return "Goodbye " + name;
